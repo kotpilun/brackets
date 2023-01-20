9731863a32cbac61134af7aee349ff68afbd6f8a
= <=>
2023-01-20 11:45:16 +0300

    feat: make task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	const stackArr = [];
+
+    for (let i = 0; i < str.length; i++) {
+        let currChar = str[i];
+        let lastStackElem = stackArr[stackArr.length - 1];
+
+        for (let j of bracketsConfig) {
+			if ((j.indexOf(currChar) == 0) && (j[0] == j[1])) {
+				lastStackElem == j[0] ? stackArr.pop() : stackArr.push(j[0]);
+				break;
+			}
+			if (j.indexOf(currChar) == 0) {
+				stackArr.push(currChar);
+				break;
+			} else if (j.indexOf(currChar) == 1) {
+				if (stackArr.length == 0) return false;
+				if (lastStackElem == j[0]) {
+					stackArr.pop();
+					break;
+				} else return false;
+			}
+        }
+    }
+    return (stackArr.length == 0) ? true : false;
 }
